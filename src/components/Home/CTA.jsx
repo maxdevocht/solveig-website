e4ce2edcf63cac61cdf5bc4f87c93306fd27343b
Max de Vocht <maxdevocht@hotmail.com>
2024-11-05 10:26:50 +0100

    Update CTA.jsx

--- src/components/Home/CTA.jsx
+++ src/components/Home/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
     <section id="cta" className="w-full">
       <CTAMarquee />
       <div className="bg-black py-12 md:py-24 lg:py-36 px-12 flex flex-col items-center gap-8">
-        <img src="./src/assets/star.svg" alt="star image" />
+        <img src="./star.svg" alt="star image" />
         <h1 className="text-white text-center text-4xl md:text-6xl font-semibold tracking-wider">
           LET&apos;S CREATE
           <br /> YOUR NEW BRAND
